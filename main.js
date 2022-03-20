18dc7a1903a79b5d89dba9bc6b54612a406b353a
xInEryo <1366749348@qq.com>
2022-03-20 20:25:00 +0800

    update

--- main.js
+++ main.js
@@ -27,46 +27,86 @@ let lastPoint = {
   y: undefined
 }
 
-canvas.addEventListener('mousedown', (e) => {
-  using = true
-  let x = e.clientX
-  let y = e.clientY
-  if (eraserEnable) {
-    context.clearRect(x - 5, y - 5, 10, 10)
-  } else {
-    lastPoint = {
-      x,
-      y
-    }
-    drawCircle(x, y, 2)
-  }
-})
+listenToUser()
 
-// onmousemove 在内部有一个响应时间，如果我们画的太快
-// 那么浏览器mousemove可能还没有触发，那么就会造成断层
-// 通过用线将两点连起来的方式，将断层缝合起来，这样看起来
-// 就不会有断连的感觉
-canvas.addEventListener('mousemove', (e) => {
-  if (using) {
+function listenToUser() {
+  if ('ontouchstart' in document) {
+    canvas.addEventListener('touchstart', (e) => {
+      using = true
+      let x = e.touches[0].clientX
+      let y = e.touches[0].clientY
+      if (eraserEnable) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        lastPoint = {
+          x,
+          y
+        }
+        drawCircle(x, y, 2)
+      }
+    })
+    canvas.addEventListener('touchmove', (e) => {
+      if (using) {
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        if (eraserEnable) {
+          context.clearRect(x - 5, y - 5, 10, 10)
+        } else {
+          let newPoint = {
+            x,
+            y
+          }
+          drawCircle(x, y, 2)
+          drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+          lastPoint = newPoint
+        }
+      }
+    })
+    canvas.addEventListener('touchend', (e) => {
+      using = false
+    })
+  }
+  canvas.addEventListener('mousedown', (e) => {
+    using = true
     let x = e.clientX
     let y = e.clientY
     if (eraserEnable) {
       context.clearRect(x - 5, y - 5, 10, 10)
     } else {
-      let newPoint = {
+      lastPoint = {
         x,
         y
       }
       drawCircle(x, y, 2)
-      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-      lastPoint = newPoint
     }
-  }
-})
+  })
 
-canvas.addEventListener('mouseup', (e) => {
-  using = false
-})
+  // onmousemove 在内部有一个响应时间，如果我们画的太快
+  // 那么浏览器mousemove可能还没有触发，那么就会造成断层
+  // 通过用线将两点连起来的方式，将断层缝合起来，这样看起来
+  // 就不会有断连的感觉
+  canvas.addEventListener('mousemove', (e) => {
+    if (using) {
+      let x = e.clientX
+      let y = e.clientY
+      if (eraserEnable) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        let newPoint = {
+          x,
+          y
+        }
+        drawCircle(x, y, 2)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+  })
+
+  canvas.addEventListener('mouseup', (e) => {
+    using = false
+  })
+}
 
 // 画圆
 function drawCircle(x, y, radius) {
@@ -90,17 +130,17 @@ function drawLine(x1, y1, x2, y2) {
   context.stroke()
 }
 
-// 监听浏览器窗口大小的变化
+// // 监听浏览器窗口大小的变化
 // window.onresize = function (e) {
-//     let pageWidth = document.documentElement.clientWidth
-//     let pageHeight = document.document   Element.clientHeight
-//     resize(pageWidth, pageHeight)
+//   let pageWidth = document.documentElement.clientWidth
+//   let pageHeight = document.documentElement.clientHeight
+//   resize(pageWidth, pageHeight)
 // }
 
-// 当窗口发送变化时，将旧画布的内容暂存下来，放到新画布上显示
+// // 当窗口发送变化时，将旧画布的内容暂存下来，放到新画布上显示
 // function resize(w, h) {
-//     let imgData = context.getImageData(0, 0, canvas.width, canvas.height)
-//     canvas.width = w
-//     canvas.height = h
-//     context.putImageData(imgData, 0, 0)
+//   let imgData = context.getImageData(0, 0, canvas.width, canvas.height)
+//   canvas.width = w
+//   canvas.height = h
+//   context.putImageData(imgData, 0, 0)
 // }
